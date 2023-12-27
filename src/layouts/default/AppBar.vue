f7b724a37ad47551ff7aafaad2bb4929f7020272
@amoon46 <atsukih46@gmail.com>
2023-12-27 20:43:15 +0900

    change-appbar

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,10 +1,13 @@
 <template>
-  <v-app-bar flat>
-    <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-6" />
-
-      Essentials Preset
-    </v-app-bar-title>
+  <v-app-bar scroll-behavior="elevate" style="height: 85px;">
+    <v-spacer>
+      <v-app-bar-title style="font-size: 30px">Title</v-app-bar-title>
+      <template v-slot:append>
+        <v-btn icon="mdi-heart"></v-btn>
+        <v-btn icon="mdi-magnify"></v-btn>
+        <v-btn icon="mdi-dots-vertical"></v-btn>
+      </template>
+    </v-spacer>
   </v-app-bar>
 </template>
 
