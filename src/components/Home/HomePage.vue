ca0bd2594172d40cf193c75d1228aa56f04a7e69
@amoon46 <atsukih46@gmail.com>
2023-12-30 08:59:26 +0900

    tmp-commit

--- src/components/Home/HomePage.vue
+++ src/components/Home/HomePage.vue
@@ -1,13 +1,39 @@
 <template>
   <v-container id="home" class="fill-height">
     <v-responsive class="align-center text-center fill-height">
-
-      <h2 class="text-h3">ATSUKI HATTORI</h2>
-
+      <v-col name="fade" appear>
+        <h2 class="text-h2 mb-4 animated-text">ATSUKI&numsp;HATTORI</h2>
+        <p class="animated-text text-grey">ENGINNER</p>
+      </v-col>
     </v-responsive>
   </v-container>
 </template>
 
+
 <script setup>
   //
 </script>
+
+<style scoped>
+@keyframes floatUp {
+  from {
+    transform: translateY(0);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+
+.animated-text {
+  animation: floatUp 2.0s ease-in-out;
+}
+
+.fade-enter-active {
+  transition: opacity 0.2s;
+}
+.fade-enter {
+  opacity: 0;
+}
+</style>
--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,14 +1,15 @@
 <template>
-    <v-app-bar color="white" elevate-on-scroll>
-      <v-spacer></v-spacer>
-      <v-btn text href="#home" class="mx-5">Home</v-btn>
-      <v-btn text href="#about" class="mx-5">About</v-btn>
-      <v-btn text href="#work" class="mx-5">Work</v-btn>
-      <v-btn text href="#contacts" class="mx-5">Contact</v-btn>
-      <v-spacer></v-spacer>
-    </v-app-bar>
+  <v-app-bar color="white" elevation="0">
+    <v-spacer></v-spacer>
+    <router-link to="#home" class="mx-5" active-class="active-link">Home</router-link>
+    <router-link to="#about" class="mx-5" active-class="active-link">About</router-link>
+    <router-link to="#work" class="mx-5" active-class="active-link">Work</router-link>
+    <router-link to="#contacts" class="mx-5" active-class="active-link">Contact</router-link>
+    <v-spacer></v-spacer>
+  </v-app-bar>
 </template>
 
+
 <script setup>
-  //
+//
 </script>
