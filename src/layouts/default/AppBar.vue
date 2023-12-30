787a373f6a875a5f8080ab839f5eb83f91103042
@amoon46 <atsukih46@gmail.com>
2023-12-30 09:01:50 +0900

    change-contacts

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -4,7 +4,7 @@
     <router-link to="#home" class="mx-5" active-class="active-link">Home</router-link>
     <router-link to="#about" class="mx-5" active-class="active-link">About</router-link>
     <router-link to="#work" class="mx-5" active-class="active-link">Work</router-link>
-    <router-link to="#contacts" class="mx-5" active-class="active-link">Contact</router-link>
+    <router-link to="#contact" class="mx-5" active-class="active-link">Contact</router-link>
     <v-spacer></v-spacer>
   </v-app-bar>
 </template>
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -2,13 +2,13 @@
   <HomePage />
   <AboutPage />
   <WorkPage />
-  <Contacts />
+  <ContactPage />
 </template>
 
 <script setup>
   import HomePage from '@/components/Home/HomePage.vue'
   import AboutPage from '@/components/About/AboutPage.vue'
   import WorkPage from '@/components/Work/WorkPage.vue'
-  import Contacts from '@/components/Contacts/Contacts.vue'
+  import ContactPage from '@/components/Contact/ContactPage.vue'
   //contactだと名前空間が重複するのでContactsに変更
 </script>
