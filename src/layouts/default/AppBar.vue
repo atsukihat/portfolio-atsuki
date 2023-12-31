8dbc466fc34776f79023c47373f067ca2096b47e
@amoon46 <atsukih46@gmail.com>
2024-01-01 08:35:02 +0900

    finish-smooth-scroll

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,20 +1,23 @@
 <template>
   <v-app-bar color="white" elevation="0">
     <v-spacer></v-spacer>
-    <v-btn href="#home" class="mx-5" >Home</v-btn>
-    <v-btn href="#about" class="mx-5">About</v-btn>
-    <v-btn href="#work" class="mx-5">Work</v-btn>
-    <v-btn href="#contact" class="mx-5">Contact</v-btn>
+    <v-btn href="#home" v-smooth-scroll="{ duration: 2000}" class="mx-5" >Home</v-btn>
+    <v-btn href="#about"  v-smooth-scroll="{ duration: 2000}" class="mx-5">About</v-btn>
+    <v-btn href="#work" v-smooth-scroll="{ duration: 2000}" class="mx-5">Work</v-btn>
+    <v-btn href="#contact" v-smooth-scroll="{ duration: 2000}" class="mx-5">Contact</v-btn>
     <v-spacer></v-spacer>
   </v-app-bar>
 </template>
 
 
 <script setup>
+import { inject, ref } from 'vue'
+  const myEl = ref(null)
+  const smoothScroll = inject('smoothScroll')
+  const scrollToMyEl = () => {
+    smoothScroll({
+      scrollTo: myEl.value,
+      hash: '#sampleHash'
+    })
+  }
 </script>
-
-<style>
-.active-link {
-  /* スタイル定義 */
-}
-</style>
