9b580a4e029e61ffbbc4eeee26762b777fa48327
@amoon46 <atsukih46@gmail.com>
2024-01-02 09:21:55 +0900

    tmp-commit

--- src/components/About/AboutPage.vue
+++ src/components/About/AboutPage.vue
@@ -1,7 +1,10 @@
 <template>
-    <v-container id="about" class="fill-height">
-        <h2 class="text-h3">服部淳生</h2>
-    </v-container>
+    <v-content id="about" class="fill-height bg-red">
+      <v-container class="align-center text-center fill-height">
+        <h2 class="text-h3">自己紹介だお</h2>
+        <p>aaa</p>
+      </v-container>
+    </v-content>
   </template>
 
   <script setup>
