b57ec98c11f53d86ef15a391fd9631888f34538a
@amoon46 <atsukih46@gmail.com>
2024-01-04 03:09:16 +0900

    complete-appBar

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,41 +1,38 @@
 <template>
   <v-container fluid  id=about class="bg-grey-lighten-5">
-    <v-row class="mt-12">
-      <v-col cols="12" class="text-center">
-        <p class="vibes-font title-size">About me</p>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="2"></v-col>
-      <!-- <v-col cols="3">
-        <v-img src="/Users/atsuki/Documents/dev/portfolio-atsuki/src/assets/paris.jpg" max-height="400"></v-img>
-      </v-col> -->
-      <v-col cols="8" class="text-center">
-        <p>福岡県出身のエンジニア。</p>
-        <p>東福岡高校を卒業後、1年の浪人期間を経て広島大学情報科学部に入学。コンピューターサイエンスについて学んだのち、広島大学大学院に進学。現在は、学習工学研究室で</p>
-      </v-col>
-      <v-col cols="2"></v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="3"></v-col>
-      <v-col cols="6">
-        <v-carousel show-arrows="hover" cycle hide-delimiters height="400">
-          <v-carousel-item>
-          <v-img src="/Users/atsuki/Documents/dev/portfolio-atsuki/src/assets/paris.jpg" max-height="400"></v-img>
-          </v-carousel-item>
+    <v-container>
+      <v-row class="mt-12">
+        <v-col cols="12" class="text-center">
+          <p class="vibes-font title-size">About me</p>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col class="text-center">
+          <p>福岡県出身のエンジニア。</p>
+          <p>東福岡高校を卒業後、1年の浪人期間を経て広島大学情報科学部に入学。コンピューターサイエンスについて学んだのち、広島大学大学院に進学。現在は、学習工学研究室で</p>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="3"></v-col>
+        <v-col cols="6">
+          <v-carousel show-arrows="hover" cycle hide-delimiters height="400">
+            <v-carousel-item>
+            <v-img src="/Users/atsuki/Documents/dev/portfolio-atsuki/src/assets/paris.jpg" max-height="400"></v-img>
+            </v-carousel-item>
 
-          <v-carousel-item>
-          <v-img src="/Users/atsuki/Documents/dev/portfolio-atsuki/src/assets/presentation.png" max-height="400"></v-img>
-          </v-carousel-item>
+            <v-carousel-item>
+            <v-img src="/Users/atsuki/Documents/dev/portfolio-atsuki/src/assets/presentation.png" max-height="400"></v-img>
+            </v-carousel-item>
 
-          <v-carousel-item>
-          <v-img src="/Users/atsuki/Documents/dev/portfolio-atsuki/src/assets/Higashi.jpg" max-height="400"></v-img>
-          </v-carousel-item>
+            <v-carousel-item>
+            <v-img src="/Users/atsuki/Documents/dev/portfolio-atsuki/src/assets/Higashi.jpg" max-height="400"></v-img>
+            </v-carousel-item>
 
-        </v-carousel>
-      </v-col>
-      <v-col cols="3"></v-col>
-    </v-row>
+          </v-carousel>
+        </v-col>
+        <v-col cols="3"></v-col>
+      </v-row>
+    </v-container>
   </v-container>
 </template>
 
--- src/components/WorkPage.vue
+++ src/components/WorkPage.vue
@@ -1,13 +1,15 @@
 <template>
   <v-container fluid  id=work class="bg-blue">
-    <v-row class="mt-12">
-      <v-col cols="12" class="text-center">
-        <p class="vibes-font title-size">Works</p>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col><v-card>aa</v-card></v-col>
-    </v-row>
+    <v-container>
+      <v-row class="mt-12">
+        <v-col cols="12" class="text-center">
+          <p class="vibes-font title-size">Works</p>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="2"><v-card>aa</v-card></v-col>
+      </v-row>
+    </v-container>
   </v-container>
 </template>
 
