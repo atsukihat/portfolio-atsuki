10a03570b45b02f646d6a32ddb92e72551652e06
@amoon46 <atsukih46@gmail.com>
2024-01-04 22:31:03 +0900

    connect-data

--- src/components/DialogCard.vue
+++ src/components/DialogCard.vue
@@ -1,14 +1,28 @@
 <template>
-    <v-card>
-    <v-card-text>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    </v-card-text>
-    <v-card-actions>
-      <v-btn color="primary" block @click="dialog = false">Close Dialog</v-btn>
-    </v-card-actions>
+  <v-card>
+    <v-container>
+      <v-row class="justify-end">
+        <v-card-actions>
+          <v-btn icon="$close" variant="text" @click="closeDialog"></v-btn>
+        </v-card-actions>
+      </v-row>
+      <v-card-text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </v-card-text>
+    </v-container>
   </v-card>
 </template>
 
 <script setup>
-//
+import { defineProps, defineEmits } from 'vue'
+
+const props = defineProps({
+  showDialog: Boolean
+})
+
+const emits = defineEmits(['update:showDialog'])
+
+const closeDialog = () => {
+  emits('update:showDialog', false)
+}
 </script>
