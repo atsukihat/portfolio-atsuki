cdad8b37104d7d729279080dce4aa739009820f9
amoon46 <atsukih46@gmail.com>
2024-03-02 17:58:06 +0900

    #ファビコンを挿入

--- src/components/SkillPage.vue
+++ src/components/SkillPage.vue
@@ -1,13 +1,31 @@
 <template>
-  <v-container fluid class="bg-grey-lighten-4">
-    <v-container id="skill" class="fill-height">
-      <v-row justify="center" align="center">
-        <p class="vibes-font title-size">Skills</p>
-      </v-row>
-    </v-container>
+  <v-container
+    fluid
+    id="about"
+    class="bg-forth"
+    :style="{ width: width + 'px', height: height + 'px' }"
+  >
+    <v-row style="height: 100%">
+      <v-col>
+        <v-row style="height: 15%">
+          <v-col align-self="center" class="text-center">
+            <PageTitle>Skills</PageTitle>
+          </v-col>
+        </v-row>
+        <v-row style="height: 25%">
+          <v-col align-self="center" class="text-center"> </v-col>
+        </v-row>
+        <v-row style="height: 60%">
+          <v-col align-self="center" class="text-center"> </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script setup>
-  //
+import { useWindowSize } from "@/composables/useWindowSize";
+import PageTitle from "@/components/shared/PageTitle";
+
+const { width, height } = useWindowSize();
 </script>
